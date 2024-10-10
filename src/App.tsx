b2fdaf0dddc9a502f71f10fd25abd237e778e723
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-10-10 17:41:52 +0800

    feat: add shadcn components + tonconnect

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { TonConnectButton, TonConnectUIProvider } from "@tonconnect/ui-react";
+
+const manifestUrl = `${
+  import.meta.env.VITE_WEBSITE_URL
+}/tonconnect-manifest.json`;
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <TonConnectButton />
+    </TonConnectUIProvider>
+  );
 }
